f90bb0b8ca8c0f25796d2008938fe8ed28e48c85
Jason Barhorst <pseudorandomcoder@gmail.com>
2018-10-07 16:26:20 -0400

    Fixed csvIO

--- 3 CSV IO/matlab/csvIO.m
+++ 3 CSV IO/matlab/csvIO.m
@@ -1,20 +1,22 @@
 clc; clear; close all;
-N = 1;
+my_path = "C:/pseudorandomcoder/data_set/matlab/dataset_out";
+data_path = "C:/pseudorandomcoder/data_set/dataset";
+
+
+N = 10;
 read_time = zeros(N, 1);
 write_time = zeros(N, 1);
-
 for i = 1:N
     tic
-    M = csvread("C:/pseudorandomcoder/data_set/dataset" + num2str(i) + ".csv");
+    M = csvread(data_path + num2str(i-1) + ".csv");
     read_time(i) = toc;
-    fprintf("read dataset%d.csv\n", i);
+    fprintf("read dataset%d.csv\n", i-1);
     
     tic
-    % csvwrite("C:/pseudorandomcoder/data_set/matlab/dataset_out" + num2str(i) + ".csv", M);
-    dlmwrite("C:/pseudorandomcoder/data_set/matlab/dataset_out" + num2str(i) + ".csv", M, 'precision', 16);
+    % dont' use csvwrite, it's limited to 5 points of precision
+    dlmwrite(my_path + num2str(i-1) + ".csv", M, 'precision', 16);
     write_time(i) = toc;
-    fprintf("wrote dataset%d_out.csv\n", i);
-    break
+    fprintf("wrote dataset%d_out.csv\n", i-1);
 end
 
 fprintf("Average Read Time: %f\n", mean(read_time))
--- 3 CSV IO/python/csvIO.py
+++ 3 CSV IO/python/csvIO.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import time
 
-N = 1
+N = 10
 read_time = []
 write_time = []
 
